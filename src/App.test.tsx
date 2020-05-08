bff412e4292b7e7c92fb562ffd686e50ff167cbe
dennie-froese <denfroese@gmail.com>
2020-05-08 18:05:18 +0100

    cleaned up App.tsx and got test working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders first try text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/first try/i);
+  expect(textElement).toBeInTheDocument();
 });
