38e6825d7077e93077c2100bbb8cb1b50f48996d
dennie-froese <denfroese@gmail.com>
2020-05-10 15:33:03 +0100

    added header component to project and implemented in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import style from "./AppStyles";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className="App" style={style.textContainer}>
-      <header className="App-header" style={style.textHeader}>
-        <p>Here it is, the first try!</p>
-        <h2
-          style={{
-            alignItems: "center",
-            color: "pink",
-            backgroundColor: "green"
-          }}
-        >
-          A very warm welcome to my personal website!
-        </h2>
-      </header>
+    <div className="App" style={style.container}>
+      <Header />
+      <p style={style.textHeader}> Text </p>
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style";
+
+export default function Header() {
+  return (
+    <div style={style.container}>
+      <p style={style.text}> Section 1</p>
+      <p style={style.text}> Section 2</p>
+      <p style={style.text}> Section 3</p>
+    </div>
+  );
+}
