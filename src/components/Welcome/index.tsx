8f73ef67d1507723e1de1ab5dc89cf6575a8ce86
dennie-froese <denfroese@gmail.com>
2020-05-11 17:36:16 +0100

    added welcome component and impleneted in app.tsx

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./style";
+
+export default function Welcome() {
+  return (
+    <div style={style.container}>
+      <p style={style.textHeader}> Hi, I'm Dennie. Nice to meet you!</p>
+      <div style={style.subContainer}>
+        <p style={style.textMain}>
+          Hi, I'm Dennie. Nice to meet you! Hi, I'm Dennie. Nice to meet you!
+          Hi, I'm Dennie. Nice to meet you! Hi, I'm Dennie. Nice to meet you!
+          Hi, I'm Dennie. Nice to meet you! Hi, I'm Dennie. Nice to meet you!
+          Hi, I'm Dennie. Nice to meet you! Hi, I'm Dennie. Nice to meet you!
+          Hi, I'm Dennie. Nice to meet you! Hi, I'm Dennie. Nice to meet you!
+          Hi, I'm Dennie. Nice to meet you!
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Welcome/style.ts
@@ -0,0 +1,24 @@
+const style = {
+  container: {
+    backgroundColor: "blue",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  subContainer: {
+    width: "50%"
+  },
+  textHeader: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  textMain: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center"
+  }
+} as { [key: string]: React.CSSProperties };
+
+export default style;
