776e7dcb8d561f85068c64a39bd5714884e8c061
dennie-froese <denfroese@gmail.com>
2020-05-12 13:44:46 +0100

    added personal logo to images folder and implemented in intro component

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import style from "./style";
+import PersonalLogo from "../../images/PersonalLogo.png";
 
 export default function Intro() {
   return (
     <div style={style.container}>
+      <div>
+        <img src={PersonalLogo} />
+      </div>
       <p style={style.textHeader}> Front-end developer and designer</p>
       <div style={style.subContainer}>
         <p style={style.textMain}>
