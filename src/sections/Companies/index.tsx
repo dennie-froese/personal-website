143111fe60ad99ecfc9c01fbee80ff74eb6371a9
dennie-froese <denfroese@gmail.com>
2020-05-19 12:32:47 +0100

    added logos to companies section

--- src/sections/Companies/index.tsx
+++ src/sections/Companies/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import style from "./style";
+import porsche from "../../images/porsche.svg";
+import santander from "../../images/santander.svg";
+import johnson from "../../images/johnson.png";
+import loreal from "../../images/loreal.png";
 
 export default function Companies() {
   return (
     <div style={style.container}>
       <p style={style.textHeader}>
-        {" "}
         I'm proud to have worked for some amazing companies and brands:
       </p>
       <div style={style.subContainer}>
-        <p style={style.textMain}>logos tbd</p>
+        <img style={style.logo} src={santander} />
+        <img style={style.logo} src={porsche} />
+        <img style={style.logo} src={loreal} />
+        <img style={style.logo} src={johnson} />
       </div>
     </div>
   );
--- src/sections/Companies/style.ts
+++ src/sections/Companies/style.ts
@@ -2,28 +2,33 @@ import { secondaryColor, secondaryTextColor } from "../../constants/styles";
 
 const style = {
   container: {
-    backgroundColor: secondaryColor,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    backgroundColor: secondaryColor,
     paddingTop: "10%",
     paddingBottom: "10%",
     borderBottom: "1px solid",
     borderBottomColor: "lightgrey"
   },
   subContainer: {
-    width: "50%"
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "70%"
   },
   textHeader: {
     fontSize: 30,
     color: secondaryTextColor,
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    paddingBottom: "5%"
   },
-  textMain: {
-    fontSize: 20,
-    color: secondaryTextColor,
-    textAlign: "center"
+  logo: {
+    width: "25%",
+    height: "25%",
+    padding: "3%"
   }
 } as { [key: string]: React.CSSProperties };
 
