a546fdbef5b3a4a1315e8f9a487554fc38b9d618
dennie-froese <denfroese@gmail.com>
2020-05-20 16:44:08 +0100

    implemented contact button with href to open mail and css styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import style from "./style";
+import "./index.css";
 
 export default function Button() {
-  return <button style={style.button}>Click me!</button>;
+  return (
+    <a
+      style={style.button}
+      href="mailto:denfroese@gmail.com?subject=Hi Dennie!"
+    >
+      <p style={style.text}>Get in touch!</p>
+    </a>
+  );
 }
