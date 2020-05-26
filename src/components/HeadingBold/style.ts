aee5d9f1baa832f47deafc72654e50fc53921a38
dennie-froese <denfroese@gmail.com>
2020-05-26 12:34:31 +0100

    added bold heading component

---
+++ src/components/HeadingBold/style.ts
@@ -0,0 +1,13 @@
+import { secondaryTextColor } from "../../constants/styles";
+
+const style = {
+  text: {
+    fontSize: 40,
+    fontFamily: "Roboto Slab",
+    color: secondaryTextColor,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+} as { [key: string]: React.CSSProperties };
+
+export default style;
