f1f073d94373138d1c1f387f53256cc174a2baab
dennie-froese <denfroese@gmail.com>
2020-05-26 16:54:49 +0100

    headingbold component now accepts optional color and fontsize props to be customisable

--- src/components/HeadingBold/style.ts
+++ src/components/HeadingBold/style.ts
@@ -2,7 +2,6 @@ import { secondaryTextColor } from "../../constants/styles";
 
 const style = {
   text: {
-    fontSize: 40,
     fontFamily: "Roboto Slab",
     color: secondaryTextColor,
     fontWeight: "bold",
