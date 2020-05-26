b4211311651538abcad15accb087a2eaefb4f113
dennie-froese <denfroese@gmail.com>
2020-05-26 08:46:09 +0100

    added useBreakpoint hook for responsive layout based on breakpoint 700

---
+++ src/hooks/useBreakpoint.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function useBreakpoint() {
+  const breakpoint = 700;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return width > breakpoint;
+}
