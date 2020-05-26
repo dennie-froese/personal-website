89f117b595996d47b018e2d28f4e29c48b249548
dennie-froese <denfroese@gmail.com>
2020-05-26 08:47:05 +0100

    implemented useBreakpoint hook in skills section to control responsive layout

--- src/sections/Skills/index.tsx
+++ src/sections/Skills/index.tsx
@@ -3,11 +3,22 @@ import style from "./style";
 import Column from "../../components/Column";
 import Separator from "../../components/Separator";
 import { content } from "./content";
+import useBreakpoint from "../../hooks/useBreakpoint";
 
 export default function Skills() {
   return (
     <div style={style.container}>
-      <div style={style.subContainer}>
+      <div
+        style={
+          useBreakpoint()
+            ? style.subContainer
+            : {
+                ...style.subContainer,
+                flexDirection: "column",
+                alignItems: "center"
+              }
+        }
+      >
         <Column content={content.columns[0]} />
         <Separator />
         <Column content={content.columns[1]} />
