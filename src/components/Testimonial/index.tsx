748532d32d103c6e758ff86e656fc6b964499c5a
dennie-froese <denfroese@gmail.com>
2020-06-03 21:35:32 +0100

    conditional fontSizes in testimonial component using useBreakpoint hook

--- src/components/Testimonial/index.tsx
+++ src/components/Testimonial/index.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import style from "./style";
 import { TestimonialType } from "../../sections/Testimonials/content";
 import Ceri from "../../images/ceri.jpeg";
 import Friede from "../../images/friede.jpg";
 import TextBody from "../TextBody";
 import HeadingBold from "../HeadingBold";
+import useBreakpoint from "../../hooks/useBreakpoint";
 
 interface Props {
   content: TestimonialType;
 }
 
 export default function Testimonial({ content }: Props) {
+  // const divRef = useRef<HTMLDivElement>(null);
+
+  // useEffect;
+
   return (
     <div style={style.container}>
       <div style={style.imageWrap}>
@@ -20,12 +25,14 @@ export default function Testimonial({ content }: Props) {
           alt=""
         />
       </div>
-      <TextBody>{content.feedback ? content.feedback : "Feedback"}</TextBody>
+      <TextBody fontSize={useBreakpoint() ? 20 : 6}>
+        {content.feedback ? content.feedback : "Feedback"}
+      </TextBody>
       <div style={style.bottom}>
-        <HeadingBold fontSize={25} margin={0}>
+        <HeadingBold fontSize={useBreakpoint() ? 25 : 15} margin={0}>
           {content.name ? content.name : "Name"}
         </HeadingBold>
-        <TextBody margin={5} fontSize={15}>
+        <TextBody margin={5} fontSize={useBreakpoint() ? 15 : 9}>
           {content.role ? content.role : "Role"}
         </TextBody>
       </div>
