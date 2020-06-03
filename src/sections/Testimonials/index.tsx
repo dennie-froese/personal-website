606566c100cece2e41606afecd87aebd98220473
dennie-froese <denfroese@gmail.com>
2020-06-03 21:36:09 +0100

    conditional width and height of testimonial carousel using useBreakpoint hook

--- src/sections/Testimonials/index.tsx
+++ src/sections/Testimonials/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./style";
 import { CarouselProvider, Slider, Slide, Dot } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
@@ -7,8 +7,37 @@ import { content } from "./content";
 import "./index.css";
 import HeadingBold from "../../components/HeadingBold";
 import TextBody from "../../components/TextBody";
+import useBreakpoint from "../../hooks/useBreakpoint";
 
 export default function Testimonials() {
+  // const [width, setWidth] = useState(window.innerWidth);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setWidth(window.innerWidth);
+  //   };
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+
+  // function lerp(start: number, end: number, t: number) {
+  //   return start * (1 - t) + end * t;
+  // }
+
+  // var slideHeightMin: number = 450;
+  // var slideHeightMax: number = 1100;
+
+  // var slideWidthMin: number = 550;
+  // var slideWidthMax: number = 1100;
+
+  // const heightSlide = lerp(slideHeightMax, slideHeightMin, width / 1600);
+  // const widthSlide = lerp(slideWidthMax, slideWidthMin, width / 1600);
+
+  // console.log(heightSlide);
+  // console.log(widthSlide);
+
   return (
     <div style={style.container}>
       <HeadingBold fontSize={30}>Testimonials</HeadingBold>
@@ -17,8 +46,8 @@ export default function Testimonials() {
       </TextBody>
       <div style={style.subContainer}>
         <CarouselProvider
-          naturalSlideWidth={600}
-          naturalSlideHeight={600}
+          naturalSlideWidth={useBreakpoint() ? 550 : 800}
+          naturalSlideHeight={useBreakpoint() ? 450 : 1200}
           totalSlides={2}
         >
           <Slider>
