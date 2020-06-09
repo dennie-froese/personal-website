f085993771b479ffdda07ce4d65511c8ae05d21c
dennie-froese <denfroese@gmail.com>
2020-06-09 08:00:39 +0100

    implemented useLerp hook in testimonial component and cleaned up

--- src/components/Testimonial/index.tsx
+++ src/components/Testimonial/index.tsx
@@ -1,35 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import style from "./style";
+
+import useLerp from "../../hooks/useLerp";
 import { TestimonialType } from "../../sections/Testimonials/content";
-import Ceri from "../../images/ceri.jpeg";
-import Friede from "../../images/friede.jpg";
 import TextBody from "../TextBody";
 import HeadingBold from "../HeadingBold";
+
+import Ceri from "../../images/ceri.jpeg";
+import Friede from "../../images/friede.jpg";
 
 interface Props {
   content: TestimonialType;
 }
 
 export default function Testimonial({ content }: Props) {
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  function lerp(start: number, end: number, t: number) {
-    return start * (1 - t) + end * t;
-  }
-
-  const fontSizeBody = Math.floor(lerp(3, 20, width / 1600));
-  const fontSizeHeader = Math.floor(lerp(10, 25, width / 1600));
-  const fontSizeSubHeader = Math.floor(lerp(8, 15, width / 1600));
+  const fontSizeBody = useLerp(3, 20);
+  const fontSizeHeader = useLerp(10, 25);
+  const fontSizeSubHeader = useLerp(8, 15);
 
   return (
     <div style={style.container}>
