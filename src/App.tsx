b573dbc43246d4d882c3d4bac915e1f8c1149658
dennie-froese <denfroese@gmail.com>
2020-06-16 19:43:56 +0100

    added github section to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Landing from "./sections/Landing";
 import Companies from "./sections/Companies";
 import Skills from "./sections/Skills";
 import Testimonials from "./sections/Testimonials";
+import GitHub from "./sections/Github";
 
 export default function App() {
   return (
     <div className="App" style={style.container}>
+      <GitHub />
       <Landing />
       <Welcome />
       <Skills />
