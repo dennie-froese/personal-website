aa70d2cd3f7482d8df66374afd0f2c9ab54ca4b9
dennie-froese <denfroese@gmail.com>
2020-06-19 17:54:14 +0100

    changed tableHeaderCell to be flex and centered items

--- src/components/TableHeaderCell/style.ts
+++ src/components/TableHeaderCell/style.ts
@@ -2,11 +2,14 @@ import { secondaryTextColor, fontStyle } from "../../constants/styles";
 
 const style = {
   text: {
+    display: "flex",
+    flexDirection: "row",
+    flex: 1,
     fontFamily: fontStyle,
+    fontSize: 15,
     color: secondaryTextColor,
     fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 20,
+    justifyContent: "center",
     padding: 10
   }
 } as { [key: string]: React.CSSProperties };
