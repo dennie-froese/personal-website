e6936cad42e3020177d8830bc8687bdb4e095362
dennie-froese <denfroese@gmail.com>
2020-06-19 17:52:47 +0100

    making tableheader component flex and centered items

--- src/components/TableHeader/style.ts
+++ src/components/TableHeader/style.ts
@@ -3,7 +3,10 @@ import { secondaryTextColor, fontStyle } from "../../constants/styles";
 const style = {
   header: {
     display: "flex",
-    width: "100%"
+    flexDirection: "row",
+    flex: 1,
+    width: "100%",
+    alignItems: "center"
   }
 } as { [key: string]: React.CSSProperties };
 
