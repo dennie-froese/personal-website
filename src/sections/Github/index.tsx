e370444adaced2615adb40368a03d4eb3152d5cb
dennie-froese <denfroese@gmail.com>
2020-06-22 17:03:20 +0100

    cleaned up git section and removed table impl

--- src/sections/Github/index.tsx
+++ src/sections/Github/index.tsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from "react";
 import style from "./style";
 import HeadingBold from "../../components/HeadingBold";
 import useBreakpoint from "../../hooks/useBreakpoint";
-import TableHeader from "../../components/TableHeader";
-import TableRow from "../../components/TableRow";
 import GitPost from "../../components/GitPost";
-import TableRowCell from "../../components/TableRowCell";
 
 export default function GitHub() {
   const [events, setEvents] = useState();
@@ -24,10 +21,6 @@ export default function GitHub() {
         The latest from my GitHub:
       </HeadingBold>
       <div style={style.subContainer}>
-        <TableHeader />
-        {/* <div style={style.subContainerTable}>
-          <table style={style.table}>
-        <tbody style={style.tableBody}> */}
         {events
           ? events.map((event: any) => {
               const date = new Date(event.created_at).toLocaleDateString();
@@ -41,24 +34,9 @@ export default function GitHub() {
                   repository={event.repo.name.split("/")[1]}
                   commits={commits}
                 />
-                // <TableRow>
-                //   <TableRowCell>{date}</TableRowCell>
-                //   <TableRowCell>{event.type}</TableRowCell>
-                //   <TableRowCell>
-                //     {event.repo.name.split("/")[1]}
-                //   </TableRowCell>
-                //   <TableRowCell>
-                //     {event.payload.commits?.map((commit: any) => (
-                //       <div>{commit.message}</div>
-                //     ))}
-                //   </TableRowCell>
-                // </TableRow>
               );
             })
           : null}
-        {/* </tbody>
-          </table>
-        </div> */}
       </div>
     </div>
   );
