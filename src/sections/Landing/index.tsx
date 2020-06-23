3556defa9c40ccd5dd27e08c0072920f03856ce2
dennie-froese <denfroese@gmail.com>
2020-06-23 20:09:30 +0100

    implemented logos in landing section

--- src/sections/Landing/index.tsx
+++ src/sections/Landing/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import style from "./style";
 import PersonalLogo from "../../images/PersonalLogo.png";
+import gmail from "../../images/gmail.png";
+import Linkedin from "../../images/Linkedin.svg";
+import Github from "../../images/Github.png";
 import Button from "../../components/Button";
 import Avatar from "../../images/Avatar-background.jpg";
 import HeadinBold from "../../components/HeadingBold";
@@ -12,7 +15,11 @@ export default function Landing() {
     <>
       <div style={style.headerContainer}>
         <img style={style.logo} src={PersonalLogo} alt="" />
-        <Button />
+        <div>
+          <img style={style.logo} src={gmail} alt="" />
+          <img style={style.logo} src={Linkedin} alt="" />
+          <img style={style.logo} src={Github} alt="" />
+        </div>
       </div>
       <div style={style.container}>
         <HeadinBold fontSize={useBreakpoint() ? 60 : 40}>
