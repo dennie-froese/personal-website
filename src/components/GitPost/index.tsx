55a44ba20b60c8d855999bdb78acc4ec7596fd29
dennie-froese <denfroese@gmail.com>
2020-06-23 08:26:48 +0100

    dynamic margins in GitPost component based on useBreakpoint

--- src/components/GitPost/index.tsx
+++ src/components/GitPost/index.tsx
@@ -13,17 +13,18 @@ interface Props {
 
 export default function GitPost({ date, event, repository, commits }: Props) {
   const font = useBreakpoint() ? 20 : 10;
+  const margin = useBreakpoint() ? 15 : 5;
   return (
     <div style={style.container}>
       <div style={style.subContainer}>
         <div style={style.header}>
-          <TextBody fontSize={font} margin={10}>
+          <TextBody fontSize={font} margin={margin}>
             {date}
           </TextBody>
-          <TextBody fontSize={font} margin={10}>
+          <TextBody fontSize={font} margin={margin}>
             {repository}
           </TextBody>
-          <TextBody fontSize={font} margin={10}>
+          <TextBody fontSize={font} margin={margin}>
             {event.split("Event")[0]}
           </TextBody>
         </div>
