ab5be35469a1bc070a9f4bd9a1520f150fce5283
dennie-froese <denfroese@gmail.com>
2020-06-24 22:16:17 +0100

    minor style changes to contactLogos component

--- src/components/ContactLogos/style.ts
+++ src/components/ContactLogos/style.ts
@@ -5,15 +5,11 @@ const style = {
     alignItems: "center"
   },
   logos: {
-    display: "flex",
-    flex: 1,
     width: 40,
     height: 40,
     padding: 10
   },
   logoGmail: {
-    display: "flex",
-    flex: 1,
     width: 38,
     height: 30,
     padding: 10
